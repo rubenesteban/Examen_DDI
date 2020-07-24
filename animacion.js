62d05788040b42a2c8eab3f42b3f0f8192a76248
rubenesteban <rubenmaya753@yahoo.com>
2020-07-24 16:35:44 -0500

    Create animacion.js
    Javascrit version 0.0

---
+++ animacion.js
@@ -0,0 +1,19 @@
+var zoom = function(){
+    var a= 11;
+    for (var i=0; i<= a; i++) {
+    var thumb = document.getElementById('thumb'+[i]);
+    thumb.className = "dos";
+    }
+    
+  console.log([i]);
+}
+
+var zuum = function(){
+    var a= 11;
+    for (var i=0; i<= a; i++) {
+    var thumb = document.getElementById('feenght'+[i]);
+    thumb.className = "dos";
+    }
+    
+  console.log([i]);
+}
